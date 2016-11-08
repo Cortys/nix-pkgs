8426bb180151c386b47bc2e44e6c3df19c9ebdc6
Clemens Damke <admin@cortys.de>
2016-11-08 01:47:14 +0100

    atom: 1.9.9 -> 1.11.2
    cherrypicked 56dfc33f49725427532d16aef27c2fe2d2413452

--- pkgs/applications/editors/atom/default.nix
+++ pkgs/applications/editors/atom/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "atom-${version}";
-  version = "1.9.9";
+  version = "1.11.2";
 
   src = fetchurl {
     url = "https://github.com/atom/atom/releases/download/v${version}/atom-amd64.deb";
-    sha256 = "1rgqajqc1z1n8ckwkxg61j0k6ridps25am54qdwjm25w53bd0z1x";
+    sha256 = "1mvlj1j0hyvm5di95nn0x99lm5arw2amm1s1va1m73zss3bzlhpm";
     name = "${name}.deb";
   };
 
